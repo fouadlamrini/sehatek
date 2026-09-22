45b3b0d5e0c6bec68c7667b6743d305fd60b1d3d
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-22 14:22:38 +0000

    await connect db

--- Sahetak_Api/src/server.js
+++ Sahetak_Api/src/server.js
@@ -6,10 +6,19 @@ const logger = require("./config/logger");
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
-connectDB();
+const startServer = async () => {
+  try {
+    // Connect to MongoDB
+    await connectDB();
 
-// Start server
-app.listen(PORT, () => {
-  logger.info(`Server running on http://localhost:${PORT}`);
-});
+    // Start server
+    app.listen(PORT, () => {
+      logger.info(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    logger.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
